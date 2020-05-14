00d91824afca460833af3da834f734b0423dca71
DeokHwan-Yun <bbayu00@gmail.com>
2020-05-14 21:26:19 +0900

    Second push on MAC!!!

--- java_api/src/java_file_io/File.java
+++ java_api/src/java_file_io/File.java
@@ -1,9 +1,14 @@
 package java_file_io;
 
 public class File {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		
 		System.out.println("Hello world");
+		
+		System.out.println("Connecting Mac to git is Success");
 	}
 }
